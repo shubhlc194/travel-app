31c64295be4f109209d6502373cdb8e57da4e7ce
shubhlc194 <shubhamkc194@gmail.com>
2025-11-24 22:34:27 +0530

    working on hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 import Image from "next/image";
-
+import Features from "./Components/Features";
+import Guide from "./Components/Guide";
+import GetApp from "./Components/GetApp";
+import Hero from "./Components/hero";
+import Camp from "./Components/camp";
 export default function Home() {
   return (
-    <h1>hello world</h1>
+   <div>
+  <Hero/>
+  <Camp/>
+   <Features/>
+   <Guide/>
+   <GetApp/>
+   
+
+   </div>
+
       )
       }
